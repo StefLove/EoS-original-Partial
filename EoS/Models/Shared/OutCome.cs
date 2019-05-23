c8b18879e4f2537c27b93ef10555ba3790a6ef57
Stefan <StefLove@users.noreply.github.com>
2019-05-24 01:44:32 +0200

    Update OutCome.cs

--- EoS/Models/Shared/OutCome.cs
+++ EoS/Models/Shared/OutCome.cs
@@ -12,6 +12,6 @@ namespace EoS.Models.Shared
         public string OutcomeName { get; set; }
 
         public virtual ICollection<IdeaCarrier.Startup> Startups { get; set; }
-        public virtual ICollection<Investor.Investment> Investments { get; set; }
+        /*public virtual ICollection<X.Y> Ys { get; set; }*/
     }
 }
